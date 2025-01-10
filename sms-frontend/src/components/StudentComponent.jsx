84fb142467639aa145f123ff1c3c4ed4b76ce26d
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-10 08:47:23 +0200

    add student form validation

--- sms-frontend/src/components/StudentComponent.jsx
+++ sms-frontend/src/components/StudentComponent.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
-
+import { createStudent } from '../services/StudentService';
+import { useNavigate } from 'react-router-dom';
 const StudentComponent = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
 
-    const handleFirstName=(e) => {
-        setFirstName(e.target.value)
-    }
+    const [errors, setErrors] = useState({
+        firstName:'',
+        lastName:'',
+        email: ''
+    })
+
+    const navigator =  useNavigate();
+    // const handleFirstName=(e) => {
+    //     setFirstName(e.target.value)
+    // }
 
     const handleLastName=(e) => {
         setLastName(e.target.value)
@@ -20,8 +28,47 @@ const StudentComponent = () => {
 
     const saveStudent=(e) => {
         e.preventDefault();
-        const student = {firstName, lastName, email};
-        console.log(student);
+
+
+        if(validateForm()){
+            const student = {firstName, lastName, email};
+            console.log(student);
+            createStudent(student).then((response) => {
+                console.log(response.data);
+            navigator('/students');
+        })
+        }
+       
+    }
+
+    function validateForm(){
+        let valid = true;
+
+        const errorsCopy={... errors}
+
+        if(firstName.trim()){
+            errorsCopy.firstName='';
+        }else {
+            errorsCopy.firstName='First name required';
+            valid= false;
+        }
+        if(lastName.trim()){
+            errorsCopy.lastName='';
+        }else {
+            errorsCopy.lastName='Last name required';
+            valid= false;
+        }
+
+         if(email.trim()){
+            errorsCopy.email='';
+        }else {
+            errorsCopy.email='Email required';
+            valid= false;
+        }
+        setErrors(errorsCopy);
+
+        return valid;
+
 
     }
   return (
@@ -41,10 +88,11 @@ const StudentComponent = () => {
                                 placeholder='Enter Student first name'
                                 name='firstName'
                                 value={firstName}
-                                className='form-control'
-                                onChange={handleFirstName}>
+                                className={`form-control ${errors.firstName ?  'is-invalid' : ''}`}
+                                onChange={(e) => setFirstName(e.target.value)}>
 
                                 </input>
+                                {errors.firstName && <div className='invalid-feedback'> {errors.firstName} </div>}
                         </div>
 
                            <div className='form-group mb-2'>
@@ -54,10 +102,10 @@ const StudentComponent = () => {
                                 placeholder='Enter Student last name'
                                 name='lastName'
                                 value={lastName}
-                                className='form-control'
+                                className={`form-control ${errors.lastName ?  'is-invalid' : ''}`}
                                 onChange={handleLastName}>
-
                                 </input>
+                                 {errors.lastName && <div className='invalid-feedback'> {errors.lastName} </div>}
                         </div>
 
                            <div className='form-group mb-2'>
@@ -67,10 +115,11 @@ const StudentComponent = () => {
                                 placeholder='Enter Student Email'
                                 name='email'
                                 value={email}
-                                className='form-control'
+                                className={`form-control ${errors.email ?  'is-invalid' : ''}`}
                                 onChange={handleEmail}>
-
                                 </input>
+                                {errors.email && <div className='invalid-feedback'> {errors.email} </div>}
+
                         </div>
 
                         <button className='btn btn-success' onClick={saveStudent}>Submit</button>
--- sms-frontend/src/services/StudentService.js
+++ sms-frontend/src/services/StudentService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/students';
+const REST_API_BASE_URL = 'http://localhost:8080/api';
 
-export const listStudents = () => axios.get(REST_API_BASE_URL);
+export const listStudents = () => axios.get(REST_API_BASE_URL + '/students');
+
+export const createStudent = (student) => axios.post(REST_API_BASE_URL + '/student', student)
