48eb26dd4d5fe994e15a2828c6b37095f11dd471
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-10 07:18:45 +0200

    added header and footer components

--- sms-frontend/src/App.js
+++ sms-frontend/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ListStudentComponent from './components/ListStudentComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterCompontn from './components/FooterCompontn';
 
 function App() {
   return (
- <ListStudentComponent />
+    <div>
+      <HeaderComponent />
+      <ListStudentComponent />
+      <FooterCompontn/>
+    </div>
+
   );
 }
 
---
+++ sms-frontend/src/components/FooterCompontn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterCompontn = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2025 kiril anastasoff</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterCompontn
