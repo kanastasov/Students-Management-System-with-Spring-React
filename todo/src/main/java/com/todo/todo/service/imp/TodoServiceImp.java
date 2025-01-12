bd0e5995825cccec3636d3f4db0b625e57c83c2d
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-12 14:13:00 +0200

    added model mapper library

--- todo/src/main/java/com/todo/todo/service/imp/TodoServiceImp.java
+++ todo/src/main/java/com/todo/todo/service/imp/TodoServiceImp.java
@@ -4,6 +4,7 @@ import com.todo.todo.dto.TodoDto;
 import com.todo.todo.entity.Todo;
 import com.todo.todo.repository.TodoRepository;
 import com.todo.todo.service.TodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,24 +14,36 @@ public class TodoServiceImp implements TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setCompleted(todoDto.isCompleted());
-        todo.setDescription(todoDto.getDescription());
+//        Todo todo = new Todo();
+//        todo.setTitle(todoDto.getTitle());
+//        todo.setCompleted(todoDto.isCompleted());
+//        todo.setDescription(todoDto.getDescription());
 
+
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
         Todo savedTodo = todoRepository.save(todo);
+//        TodoDto savedDto = new TodoDto();
+//        savedDto.setId(savedTodo.getId());
+//        savedDto.setCompleted(savedTodo.isCompleted());
+//        savedDto.setDescription(savedTodo.getDescription());
+//        savedDto.setTitle(savedTodo.getTitle());
 
-        TodoDto savedDto = new TodoDto();
-        savedDto.setId(savedTodo.getId());
-        savedDto.setCompleted(savedTodo.isCompleted());
-        savedDto.setDescription(savedTodo.getDescription());
-        savedDto.setTitle(savedTodo.getTitle());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
+
+        return savedTodoDto;
+    }
 
 
-        return savedDto;
+    public TodoServiceImp(TodoRepository todoRepository, ModelMapper modelMapper) {
+        this.todoRepository = todoRepository;
+        this.modelMapper = modelMapper;
     }
 }
