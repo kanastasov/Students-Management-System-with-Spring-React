298597f2e17081d7621bc09c4835396a5c2bbc32
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-13 12:27:12 +0200

    get all todos from db

--- todo-frontend/src/components/ListTodoComponent.jsx
+++ todo-frontend/src/components/ListTodoComponent.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { getAllTodos } from '../services/TodoService';
 
 const ListTodoComponent = () => {
+
+  //   const data = [
+  //   {
+  //     "id": 1,
+  //     "title": "todo title",
+  //     "description": "to do description",
+  //     "completed": false
+
+  //   },
+  //     {
+  //     "id": 2,
+  //     "title": "todo title2",
+  //     "description": "to do description2",
+  //     "completed": false
+
+  //   },
+  //     {
+  //     "id": 3,
+  //     "title": "todo title3",
+  //     "description": "to do description3",
+  //     "completed": false
+
+  //   }
+  // ]
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    listTodos();
+  }, [])
+
+
+  function listTodos() {
+    getAllTodos().then((response) => {
+        setTodos(response.data);
+    }).catch(err => {
+        console.log(err);
+    });
+  }
+
   return (
-    <div>ListTodoComponent</div>
+    // <h1>sssss</h1>
+     <div className="container">
+        <h2 className='text-center'>List of todos</h2>
+          <table className='table table-bordered table-striped'>
+            <thead>
+              <tr>
+                  <th>Todo Title  </th>
+                     <th>Todo description  </th>
+                        <th>Todo completed  </th>
+              </tr>
+                  </thead>
+              <tbody>
+                {
+                  todos.map( (todo) =>  
+                    <tr key={todo.id}>
+                      <td> {todo.title} </td>
+                      <td>{todo.description}</td>
+                      <td>{todo.completed ? 'Yes' : 'No'}</td>
+                  </tr>
+                  )
+                }
+                  
+              </tbody>
+        
+          </table>
+    </div>
   )
 }
 
---
+++ todo-frontend/src/services/TodoService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BASE_REST_API_URL = 'http://localhost:8080/api/todos';
+
+// export function getAllTodos(){
+//     return axios.get(BASE_REST_API_URL);
+// }
+
+export const getAllTodos = () => axios.get(BASE_REST_API_URL);
+
