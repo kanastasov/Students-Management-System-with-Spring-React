f321463755857750898f6f8cc3c5959f4548eca0
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-13 09:05:40 +0200

    added list to do comp

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import ListTodoComponent from './components/ListTodoComponent';
+import { useState } from 'react';
 
 function App() {
+
+  const data = [
+    {
+      "id": 1,
+      "title": "todo title",
+      "description": "to do description",
+      "completed": false
+
+    },
+      {
+      "id": 2,
+      "title": "todo title2",
+      "description": "to do description2",
+      "completed": false
+
+    },
+      {
+      "id": 3,
+      "title": "todo title3",
+      "description": "to do description3",
+      "completed": false
+
+    }
+  ]
+
+  const [todos, setTodos] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h2 className='text-center'>List of todos</h2>
+          <table className='table table-bordered table-striped'>
+            <thead>
+              <tr>
+                  <th>Todo Title  </th>
+                     <th>Todo description  </th>
+                        <th>Todo completed  </th>
+              </tr>
+                  </thead>
+              <tbody>
+                {
+                  todos.map( (todo) =>  
+                    <tr key={todo.id}>
+                      <td> {todo.title} </td>
+                      <td>{todo.description}</td>
+                      <td>{todo.completed ? 'Yes' : 'No'}</td>
+                  </tr>
+                  )
+                }
+                  
+              </tbody>
+        
+          </table>
+        <ListTodoComponent />
     </div>
   );
 }
---
+++ todo-frontend/src/components/ListTodoComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListTodoComponent = () => {
+  return (
+    <div>ListTodoComponent</div>
+  )
+}
+
+export default ListTodoComponent
