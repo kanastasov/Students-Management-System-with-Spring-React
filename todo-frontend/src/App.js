a6ff1d8a78233c0fb4bed692ec86b85a6ffca0c4
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-13 12:48:12 +0200

    react router dom

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -3,14 +3,23 @@ import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListTodoComponent from './components/ListTodoComponent';
 
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 function App() {
 
   
   return (
     <div>
+      <BrowserRouter>
         <HeaderComponent />
-        <ListTodoComponent />
+
+      <Routes>
+        <Route path = '/' element = {    <ListTodoComponent />}></Route>
+        <Route path = '/todos' element = {<ListTodoComponent />}></Route>
+      </Routes>
+    
         <FooterComponent />
+      </BrowserRouter>
     </div>
   );
 }
