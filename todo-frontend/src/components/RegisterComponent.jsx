cabcfa7c4af65d7864ed0682d97751ce20496cce
kirilanastasoff <kirilanastasoff@gmail.com>
2025-01-15 07:11:56 +0200

    user registration form

--- todo-frontend/src/components/RegisterComponent.jsx
+++ todo-frontend/src/components/RegisterComponent.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterComponent = () => {
-  return (
-    <div>RegisterComponent</div>
+
+  const[name,setName] = useState('');
+  const[username,setUsername] = useState('');
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+
+  function handleRegistrationForm(e){
+    e.preventDefault();
+
+    const register = {name, username,email,password};
+    console.log(register);
+  }
+
+    return (
+    <div className='container'>
+      <br/>
+      <div className='row'>
+        <div className='col-md-6 offset-md-6'>
+          <div className='card'>
+            <div className='car-header'>
+              <h2 className='text-center'>User Registration form</h2>
+            </div>
+
+            <div className='card-body'>
+              <form>
+                <div className='row mb-3'>
+                  <label className='col-md-3 control-label'> Name</label>
+                  <div className='col-md-9'>
+                    <input
+                      type='text'
+                      name='name'
+                      className='form-control'
+                      placeholder='Enter name'
+                      value = {name}
+                      onChange={(e) => setName(e.target.value)}
+                    ></input>
+                  </div>
+                </div>
+
+                  <div className='row mb-3'>
+                  <label className='col-md-3 control-label'> Username</label>
+                  <div className='col-md-9'>
+                    <input
+                      type='text'
+                      name='username'
+                      className='form-control'
+                      placeholder='Enter username'
+                      value = {username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    ></input>
+                  </div>
+                </div>
+
+                <div className='row mb-3'>
+                  <label className='col-md-3 control-label'> Email</label>
+                  <div className='col-md-9'>
+                    <input
+                      type='text'
+                      name='email'
+                      className='form-control'
+                      placeholder='Enter email'
+                      value = {email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    ></input>
+                  </div>
+                </div>
+
+                         <div className='row mb-3'>
+                  <label className='col-md-3 control-label'> Password</label>
+                  <div className='col-md-9'>
+                    <input
+                      type='text'
+                      name='password'
+                      className='form-control'
+                      placeholder='Enter password'
+                      value = {password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                  </div>
+                </div>
+
+              <div className='form-group mb-3'>
+                <button className='btn btn-primary' onClick={(e) => handleRegistrationForm(e)}>Submit</button>
+                
+              </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
